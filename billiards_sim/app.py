8e44a1a2de4c67c8e42feabd0922891739c5d4e5
Erik Hofmayer <ehofmayer@outlook.de>
2021-02-05 11:58:41 +0100

    Added invert_y option

--- billiards_sim/app.py
+++ billiards_sim/app.py
@@ -8,6 +8,7 @@ HEIGHT_KEY = 'height'
 BALL_RADIUS_KEY = 'radius'
 FPS_KEY = 'fps'
 TPF_KEY = 'tpf'
+INVERT_Y_KEY = 'invert_y'
 BALLS_KEY = 'balls'
 POS_KEY = 'pos'
 VEL_KEY = 'vel'
@@ -38,6 +39,7 @@ def run():
     tpf = data.get(TPF_KEY, 10)
     width = data.get(WIDTH_KEY, 1270)
     height = data.get(HEIGHT_KEY, 2540)
+    invert_y = data.get(INVERT_Y_KEY, True)
 
     friction = data.get(FRICTION_KEY, 0)
 
@@ -45,7 +47,11 @@ def run():
     ui_pockets = []
     if POCKETS_KEY in data:
         for pocket in data[POCKETS_KEY]:
-            pocket = physics.Pocket(pocket[POCKET_SIDE_KEY], pocket[POCKET_DIAMETER_KEY], pocket.get(POCKET_POS_KEY, None))
+            pos = pocket.get(POCKET_POS_KEY, None)
+            side = pocket[POCKET_SIDE_KEY]
+            if invert_y and (pos == 'left' or pos == 'right'):
+                pos = -pos
+            pocket = physics.Pocket(side, pocket[POCKET_DIAMETER_KEY], pos)
             pockets.append(pocket)
             ui_pockets.append(window.Pocket(pocket))
 
@@ -55,10 +61,14 @@ def run():
             ball = str(action[ACTIONS_BALL_KEY])
             vel = action.get(ACTIONS_VEL_KEY, None)
             vel = None if vel is None else physics.Vector(vel[0], vel[1])
+            if vel is not None and invert_y:
+                vel.y = -vel.y
             push = action.get(ACTIONS_PUSH_KEY, None)
             if push is not None:
                 pushed_ball = push[ACTIONS_PUSH_BALL_KEY]
                 dest = push[ACTIONS_PUSH_DEST_KEY]
+                if invert_y:
+                    dest[1] = height - dest[1]
                 abs_vel = push[ACTIONS_PUSH_VEL_KEY]
                 rail = push.get(ACTIONS_PUSH_RAIL_KEY, None)
                 push = physics.PushActionData(pushed_ball, physics.Point(dest[0], dest[1]), abs_vel, rail)
@@ -70,6 +80,9 @@ def run():
         for ball in data[BALLS_KEY]:
             pos = ball[POS_KEY]
             vel = ball[VEL_KEY]
+            if invert_y:
+                pos[1] = height - pos[1]
+                vel[1] = -vel[1]
             color = ball[COLOR_KEY]
             name = ball.get(BALL_NAME_KEY, None)
             ph_ball = physics.Ball(ball_radius, physics.Point(pos[0], pos[1]), physics.Vector(vel[0], vel[1]), name)
